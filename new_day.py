f823402e8c2343973cbe6a60453cc7954237de70
ranpo-edogawa <umarhon3005@gmail.com>
2023-10-17 22:05:19 -0700

    first_commit

---
+++ new_day.py
@@ -0,0 +1,20 @@
+# class Number:
+#     def __init__(self) -> None:
+#         self.dct = ['(', ')', '-', '-', '-']
+#         pass
+    
+    
+#     def check_true_phone_number(self, phone_number):
+#         if len(phone_number) != 18:
+#             print("Your phone number is not correct", len(phone_number))
+#             return False
+#         for i in self.dct:
+#             if i not in phone_number:
+#                 print("Your phone number is not correct", i)
+#                 return False
+#         return True 
+    
+    
+# mobile_phone = Number()
+# mobile_phone_number = input("Enter your mobile phone number: ")
+# print(mobile_phone.check_true_phone_number(mobile_phone_number))
