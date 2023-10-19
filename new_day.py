30f1004ac4dd9b7e6891afb84e485a72796dd410
ranpo-edogawa <umarhon3005@gmail.com>
2023-10-18 21:38:08 -0700

    whatever

--- new_day.py
+++ new_day.py
@@ -18,3 +18,39 @@
 # mobile_phone = Number()
 # mobile_phone_number = input("Enter your mobile phone number: ")
 # print(mobile_phone.check_true_phone_number(mobile_phone_number))
+
+
+
+# class Birthday:
+#     def __init__(self) -> None:
+#         pass
+    
+
+#     def check_birthday(self, birthday, name):
+#         if birthday % 2 == 0:
+#             text = f'{birthday} Happy birthday {name}! {birthday}'
+#             self.print_birthday_1(text)
+#         else:
+#             text = f'{birthday} Happy birthday {name}! {birthday}'
+#             self.print_birthday_2(text)
+        
+#     def print_birthday_1(self, tabrik):
+        
+#         print('#'*(len(tabrik)+4))
+#         print('#', tabrik, '#')
+#         print('#'*(len(tabrik)+4))
+        
+        
+#     def print_birthday_2(self, tabrik):
+        
+#         print('*'*(len(tabrik)+4))
+#         print('*', tabrik, '*')
+#         print('*'*(len(tabrik)+4))
+        
+        
+        
+# person = Birthday()
+# person.check_birthday(24, 'Levi')
+
+
+
